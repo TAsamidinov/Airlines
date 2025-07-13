3f030eb1d1caacb74ea4857f747949b734147196
Temirlan <temirlan.asamidinov@gmail.com>
2025-07-13 07:38:41 -0500

    Added class into admin

--- flights/admin.py
+++ flights/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from .models import Airport, Flight, Passengers
 
 # Register your models here.
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("code", "city")
 
-admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passengers)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+    list_filter = ("origin", "destination", "duration")
+    search_fields = ("origin__city", "destination__city")
+
+class PassengersAdmin(admin.ModelAdmin):
+    list_display = ("first", "last")
+    search_fields = ("first", "last")
+    filter_horizontal = ("flights",)
+
+admin.site.register(Airport, AirportAdmin)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passengers, PassengersAdmin)
