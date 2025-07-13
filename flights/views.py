27e2414b343e9d083a466f851d30b0347a545d92
Temirlan <temirlan.asamidinov@gmail.com>
2025-07-13 07:21:51 -0500

    Booking become available

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -13,8 +15,23 @@ def flight(request, flight_id):
         return render(request, "flights/flight.html", {
             "flight": flight,
             "passengers": flight.passengers.all(),
+            "non_passengers": Passengers.objects.exclude(flights=flight).all(),
         })
     except Flight.DoesNotExist:
         return render(request, "flights/error.html", {
             "message": "Flight not found."
         })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        try:
+            flight = Flight.objects.get(pk=flight_id)
+            passenger = Passengers.objects.get(pk=int(request.POST["passenger"]))
+            flight.passengers.add(passenger)
+
+            return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+
+        except (Flight.DoesNotExist, Passengers.DoesNotExist):
+            return render(request, "flights/error.html", {
+                "message": "Flight or passenger not found."
+            })
